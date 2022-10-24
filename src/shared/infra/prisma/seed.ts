0e3236d71d08c9e17f2ebb089bf703ec3a00826c
murillolamego <murillolamegolopes@live.com>
2022-10-24 17:46:15 -0300

    Seed for PrismaORM

---
+++ src/shared/infra/prisma/seed.ts
@@ -0,0 +1,41 @@
+import { hash } from "bcrypt";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main(): Promise<void> {
+  const saltRounds = 10;
+  const password = await hash("123", saltRounds);
+
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@test.com" },
+    update: {},
+    create: {
+      email: "admin@test.com",
+      name: "admin",
+      password,
+      admin: true,
+    },
+  });
+
+  const notAdmin = await prisma.user.upsert({
+    where: { email: "notadmin@test.com" },
+    update: {},
+    create: {
+      email: "notadmin@test.com",
+      name: "notadmin",
+      password,
+    },
+  });
+  console.log({ admin, notAdmin });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
